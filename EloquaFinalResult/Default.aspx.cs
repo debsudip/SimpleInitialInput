8e8cc3ff132220cfebf6fd4c5ec340ac05097ca5
Sudip Kumar Deb <debsudip@gmail.com>
2013-06-18 12:20:18 +0530

    Initial Commit

---
+++ EloquaFinalResult/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EloquaFinalResult
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MessageBox ("Reached the Initial Input :)");
+        }
+
+        #region Message Box
+        /// <summary>
+        /// Generic message box
+        /// </summary>
+        /// <param name="strMsg">String Message</param>
+        public void MessageBox(string strMsg)
+        {
+            Controls.Add(new LiteralControl("<script language='javascript'> window.alert('" + strMsg.Replace("'", "\\'") + "')</script>"));
+        }
+        #endregion
+
+    }
+}
---
+++ EloquaFinalResult/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EloquaFinalResult")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CTS")]
+[assembly: AssemblyProduct("EloquaFinalResult")]
+[assembly: AssemblyCopyright("Copyright © CTS 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("24ce64e0-2014-4cc8-83ff-e3d6d1d2cc8a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
